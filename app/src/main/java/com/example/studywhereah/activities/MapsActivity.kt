09b1b694a5eee7789dc9cec0596117baaa7cf1ac
ngzhenteng <ngzhenteng@gmail.com>
2020-06-24 12:52:28 +0800

    search Bar partially completed
    Search bar has an onClickListener which fires up the autocomplete search widget, upon selecting a place, the map pans to the selected location.
    Clicking on the marker will bring up the "navigate to" and "show in google maps" buttons in the bottom right corner as well.

--- app/src/main/java/com/example/studywhereah/activities/MapsActivity.kt
+++ app/src/main/java/com/example/studywhereah/activities/MapsActivity.kt
@@ -8,18 +8,14 @@ import android.content.Intent
 import android.location.Location
 import android.location.LocationManager
 import android.net.Uri
-import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
-import android.util.Log
 import android.view.View
-import android.widget.EditText
+import android.view.ViewTreeObserver
 import android.widget.Toast
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AlertDialog
-import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentActivity
 import com.example.studywhereah.R
-import com.google.android.gms.common.api.Status
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -27,24 +23,26 @@ import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.LatLngBounds
 import com.google.android.gms.maps.model.MarkerOptions
 import com.google.android.libraries.places.api.Places
 import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.api.model.RectangularBounds
 import com.google.android.libraries.places.widget.Autocomplete
 import com.google.android.libraries.places.widget.AutocompleteActivity
 import com.google.android.libraries.places.widget.AutocompleteSupportFragment
-import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
 import com.google.android.libraries.places.widget.model.AutocompleteActivityMode
 import com.karumi.dexter.Dexter
 import com.karumi.dexter.MultiplePermissionsReport
 import com.karumi.dexter.PermissionToken
 import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 import kotlinx.android.synthetic.main.activity_maps.*
-import java.util.*
 
-class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapsActivity : FragmentActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
+    private var mBounds: LatLngBounds.Builder = LatLngBounds.Builder()
+    private lateinit var mapFragment: SupportMapFragment
     private lateinit var mFusedLocationClient : FusedLocationProviderClient
 
     private var currentLatitude : Double = 0.0
@@ -53,15 +51,25 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     // editText is a var referring to the searchbar element searchbar_edit_text
 //    private lateinit var editText: EditText
 
-    companion object {
-        private const val PLACE_AUTOCOMPLETE_REQUEST_CODE = 1
-    }
+//    companion object {
+//        private const val PLACE_AUTOCOMPLETE_REQUEST_CODE = 1
+//    }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+//        var COUNTRIES = arrayOf("Singapore", "I DONT KNOW", "YKNOW")
+
+        //AutoCompleteTextView irrelevant for now
+//        var adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
+//        COUNTRIES)
+//        actv_searchBar.setAdapter(adapter)
+//        actv_searchBar.setOnItemClickListener(object: AdapterView.OnItemClickListener {
+//
+//        })
 
         /**
          * Initialize the places sdk if it is not initialized earlier using the api key.
@@ -75,17 +83,17 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
         // Initialize places, we may need the places api key(diff from google maps android sdk api key
         // for the string parameter
-//        Places.initialize(applicationContext, resources.getString(R.string.google_maps_api_key))
 
-        // Set EditText non focusable
-        searchbar_edit_text.isFocusable = false
-        searchbar_edit_text.setOnClickListener(object: View.OnClickListener {
+        // Set EditText non focusable, the code below is to initiate an autocomplete activity
+        // when the searchbar is clicked on.
+        tv_search.setOnClickListener(object: View.OnClickListener {
             public override fun onClick(v: View?) {
                 var fieldList: List<Place.Field> = listOf(Place.Field.ADDRESS,
                     Place.Field.LAT_LNG,
                     Place.Field.NAME)
 
                 var intent = Autocomplete.IntentBuilder(AutocompleteActivityMode.OVERLAY, fieldList)
+                    .setCountry("SG")
                     .build(this@MapsActivity)
 
                 startActivityForResult(intent, 100)
@@ -113,12 +121,54 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 //        })
 
  */
+        // Code pertaining to Searchview
 
-        settleLocation()
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager
-                .findFragmentById(R.id.map) as SupportMapFragment
+//        sv_location.suggestionsAdapter
+//        //The code below enables an integrated searchbar that tallies with the geocoder database.
+//        sv_location.setOnQueryTextListener(object: SearchView.OnQueryTextListener {
+//
+//            override fun onQueryTextSubmit(query: String?): Boolean {
+//                var location = sv_location.query.toString()
+//                lateinit var addressList: List<Address>
+//                if (location != null || !location.equals("")) {
+//                    var geocoder = Geocoder(this@MapsActivity)
+//                    try {
+//                        addressList = geocoder.getFromLocationName(location, 1)
+//                    } catch (e: IOException) {
+//                        e.printStackTrace()
+//                    }
+//                    var address = addressList.get(0)
+//                    var coordinates = LatLng(address.latitude, address.longitude)
+//                    mMap.addMarker(MarkerOptions().position(coordinates).title(location))
+//                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(coordinates, 10.0F))
+//                }
+//
+//                return false
+//
+//            }
+//
+//            override fun onQueryTextChange(newText: String?): Boolean {
+//                //this is where i add suggestions
+//
+//                var fieldList: List<Place.Field> = listOf(Place.Field.ADDRESS,
+//                    Place.Field.LAT_LNG,
+//                    Place.Field.NAME)
+//
+//                var intent = Autocomplete.IntentBuilder(AutocompleteActivityMode.OVERLAY, fieldList)
+//                    .build(this@MapsActivity)
+//
+//                startActivityForResult(intent, 100)
+//                return false
+//            }
+//        })
         mapFragment.getMapAsync(this)
+
+
+//        settleLocation()
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+//        val mapFragment = supportFragmentManager
+//                .findFragmentById(R.id.map) as SupportMapFragment
+//        mapFragment.getMapAsync(this)
 
 
         btn_get_place1.setOnClickListener {
@@ -129,15 +179,23 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         }
     }
 
-    // overidding this function is apparently crucial
+    private fun addPointToViewPort(newPoint: LatLng) {
+        mBounds.include(newPoint)
+        mMap.animateCamera(CameraUpdateFactory.newLatLngBounds(mBounds.build(), 13))
+    }
+
+    // overidding this function is apparently crucial for Places Autocomplete
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == 100 && resultCode == Activity.RESULT_OK) {
             //When success
             //Initialize place
             var place = Autocomplete.getPlaceFromIntent(data!!)
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(place.latLng, 15.0f))
+            mMap.addMarker(MarkerOptions().position(place.latLng!!)
+                .title(place.name + ", CHECK IT OUT!" ))
             //Set address on searchbar_edit_text
-            searchbar_edit_text.setText(place.address)
+            tv_search.setText(place.name)
             //We can get the locality name, lat and long from place.
         } else if (resultCode == AutocompleteActivity.RESULT_ERROR) {
             //When error initialize status
@@ -159,9 +217,20 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
      */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
+        mMap.isMyLocationEnabled = true
+        //code below is to set the padding of the "interactable" portion of the map :)
+        tv_search.viewTreeObserver.addOnGlobalLayoutListener(object:
+            ViewTreeObserver.OnGlobalLayoutListener {
+            override fun onGlobalLayout() {
+                mMap.setPadding(0, tv_search.height + 40, 0, 0)
+            }
+        })
+        mMap?.apply {settleLocation()}
 
     }
 
+    // settleLocation requests for and checks for all permissions to be granted
+    // then sets the currLong and currLat to be that location.
     private fun settleLocation() {
         if (!isLocationEnabled()) {
             Toast.makeText(
@@ -238,19 +307,15 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     }
 
     private fun updateMapLocation(location: Location?) {
-
         currentLatitude = location?.latitude!!
         currentLongitude = location?.longitude!!
-
 
         var location = LatLng(
             location?.latitude ?: 0.0,
             location?.longitude ?: 0.0)
 
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(location))
-
-        mMap.moveCamera(CameraUpdateFactory.zoomTo(15.0f))
-        mMap.addMarker(MarkerOptions().position(location).title("Your current location"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 15.0f))
+//        mMap.addMarker(MarkerOptions().position(location).title("Your current location"))
     }
 
 }
